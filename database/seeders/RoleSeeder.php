8f4f9fc32634112562bc10941b14f47697d5e275
OmarNasab <77580153+OmarNasab@users.noreply.github.com>
2022-06-04 14:38:09 +0800

    Update RoleSeeder.php

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -2,9 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\Role;
 use DB;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class RoleSeeder extends Seeder
@@ -17,27 +15,25 @@ class RoleSeeder extends Seeder
     public function run()
     {
         DB::table("roles")->insert([
-            "name" => "admin",
-            "permissions" => [
-                "permissions" => [
-                    "add_users",
-                    "edit_users",
-                    "delete_users",
-                    "show_users",
-                    "add_drivers",
-                    "edit_drivers",
-                    "delete_drivers",
-                    "show_drivers",
-                    "show_expenses",
-                    "verify_expenses",
-                    "add_mission",
-                    "show_mission",
-                    "add_roles",
-                    "edit_roles",
-                    "delete_roles",
-                    "show_roles",
-                ]
-            ]
+            "name"=>"admin",
+            "permissions"=> json_encode('[
+                "add_users",
+                "edit_users",
+                "delete_users",
+                "show_users",
+                "add_drivers",
+                "edit_drivers",
+                "delete_drivers",
+                "show_drivers",
+                "show_expenses",
+                "verify_expenses",
+                "add_mission",
+                "show_mission",
+                "add_roles",
+                "edit_roles",
+                "delete_roles",
+                "show_roles",
+            ]')
         ]);
 
     }
