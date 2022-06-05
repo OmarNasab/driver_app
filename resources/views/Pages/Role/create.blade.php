b38a14eff9fc5dd10214bfd0104bc94a43ac2244
OmarNasab <77580153+OmarNasab@users.noreply.github.com>
2022-06-06 01:56:51 +0800

    add role input styling

--- resources/views/Pages/Role/create.blade.php
+++ resources/views/Pages/Role/create.blade.php
@@ -20,7 +20,7 @@
                 @foreach($permissions->getCurrentPermissions() as $permission)
                 <div class="row-span-1">
                     <label>
-                        <input type="checkbox" name="permissions[]" value="{{$permission}}">
+                        <input class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" type="checkbox" name="permissions[]" value="{{$permission}}">
                         {{$permissions->getRoleName($permission)}}
                     </label>
                 </div>
