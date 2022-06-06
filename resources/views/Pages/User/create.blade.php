b3e6632855527ee560082056fe9ab09006f1876e
OmarNasab <77580153+OmarNasab@users.noreply.github.com>
2022-06-06 18:16:34 +0800

    Update create.blade.php

--- resources/views/Pages/User/create.blade.php
+++ resources/views/Pages/User/create.blade.php
@@ -24,7 +24,7 @@
 
                     <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required></x-input>
                 </div>
-                <label for="role" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Select User's Role</label>
+                <label for="role" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Role</label>
                 <select id="role" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                     <option disabled selected>Choose Role</option>
                     @foreach($roles as $role)
