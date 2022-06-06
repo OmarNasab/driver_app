4ac69a8c30c3bef901380063fcc71810c5f1f73f
OmarNasab <77580153+OmarNasab@users.noreply.github.com>
2022-06-06 18:56:58 +0800

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,7 @@ class User extends Authenticatable
         'name',
         'email',
         "role_id",
+        "password"
     ];
     /**
      * The attributes that should be hidden for serialization.
