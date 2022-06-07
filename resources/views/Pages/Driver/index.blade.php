2132d23c086efd740bff5825706803c7ed9a67d0
OmarNasab <77580153+OmarNasab@users.noreply.github.com>
2022-06-07 14:48:25 +0800

    Update index.blade.php

--- resources/views/Pages/Driver/index.blade.php
+++ resources/views/Pages/Driver/index.blade.php
@@ -1,9 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
         <div class="flex flex-row w-full justify-between">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Driver') }}->{{__("Driver List")}}
-        </h2>
             <div>
                 <x-link link="driver.create">Add Driver</x-link>
             </div>
