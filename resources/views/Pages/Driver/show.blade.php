191211438606447229172973da9213ead01ff260
OmarNasab <77580153+OmarNasab@users.noreply.github.com>
2022-06-12 23:40:09 +0800

    css fix

--- resources/views/Pages/Driver/show.blade.php
+++ resources/views/Pages/Driver/show.blade.php
@@ -7,13 +7,13 @@
             </div>
         </div>
         <div class="container mt-4">
-            <div class="ml-4 grid grid-rows-3 grid-cols-5 grid-flow-col gap-4">
+            <div class="ml-4 grid grid-rows-4 grid-cols-5 grid-flow-col gap-4">
                 <div class="row-span-4">
                     <img class="inline" src="{{URL::asset("storage")."/".$driver->image}}" width="400" alt="document" >
                 </div>
                 <div class="row-span-1"><span class="font-bold">Driver Name:</span> {{$driver->full_name}}</div>
                 <div class="row-span-1"><span class="font-bold">Email:</span> {{$driver->email}}</div>
-                <div class="row-span-1"><span class="font-bold">Phone Number:</span> {{$driver->phone}}</div>
+                <div class="row-span-2"><span class="font-bold">Phone Number:</span> {{$driver->phone}}</div>
                 <div class="row-span-1"><span class="font-bold">UAE ID:</span> {{$driver->uaeID}}</div>
                 <div class="row-span-1"><span class="font-bold">Status:</span>
                     @if($driver->status==0)
