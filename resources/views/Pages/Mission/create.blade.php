e46fda24cc24e4b53bd898175bad13708e4b6c84
OmarNasab <77580153+OmarNasab@users.noreply.github.com>
2022-06-12 17:03:19 +0800

    Update create.blade.php

--- resources/views/Pages/Mission/create.blade.php
+++ resources/views/Pages/Mission/create.blade.php
@@ -93,7 +93,7 @@
                     <div class="mb-6">
                         <label for="address_address"
                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Stop Address</label>
-                        <input type="text" id="address_input" name="address_address"
+                        <input type="text" id="address-input" name="address_address"
                                class="map-input bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                placeholder="Address" required>
                     </div>
