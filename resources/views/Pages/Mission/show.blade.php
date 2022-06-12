4be0e61c3bdc21eb5d39c99d160eb2a003471b60
OmarNasab <77580153+OmarNasab@users.noreply.github.com>
2022-06-12 14:31:26 +0800

    add destinations table

--- resources/views/Pages/Mission/show.blade.php
+++ resources/views/Pages/Mission/show.blade.php
@@ -87,6 +87,35 @@ $directions=[];
                         </div>
                         <label for="default-range" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Default range</label>
                         <input id="default-range" type="range" value="1" min="1" max="{{count($directions)}}" onchange="changeMarker()" class="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700">
+                        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                            <tr>
+                                <th scope="col" class="px-6 py-3">
+                                    Destination
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Supervisor
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    <span class="sr-only">View</span>
+                                </th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($mission->direction as $stop)
+                                <tr class="border-b dark:bg-gray-800 dark:border-gray-700 odd:bg-white even:bg-gray-50 odd:dark:bg-gray-800 even:dark:bg-gray-700">
+                                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                                        {{$stop["name"]}}
+                                    </th>
+                                    <td class="px-6 py-4">
+                                    </td>
+                                    <td class="px-6 py-4 text-right">
+                                        <a href="" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">View</a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
                     </div>
                     @endif
                 </div>
