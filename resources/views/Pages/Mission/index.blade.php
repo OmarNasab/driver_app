5a4dfde421e86d16db442cd1e1054c35501fb557
OmarNasab <77580153+OmarNasab@users.noreply.github.com>
2022-06-15 23:48:48 +0800

    test

--- resources/views/Pages/Mission/index.blade.php
+++ resources/views/Pages/Mission/index.blade.php
@@ -149,7 +149,7 @@
         // options with default values
         const options = {
             defaultTabId: 'settings',
-            activeClasses: 'text-red-200 hover:text-gray-600 dark:text-blue-500 dark:hover:text-blue-400 border-gray-600 dark:border-blue-500',
+            activeClasses: 'text-blue-600 hover:text-blue-600 dark:text-blue-500 dark:hover:text-blue-400 border-blue-600 dark:border-blue-500',
             inactiveClasses: 'text-gray-500 hover:text-gray-600 dark:text-gray-400 border-gray-100 hover:border-gray-300 dark:border-gray-700 dark:hover:text-gray-300',
             onShow: () => {
                 console.log('tab is shown');
