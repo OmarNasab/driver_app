62107c8d9d380456d247e827ed52e60a3c8a8bf8
OmarNasab <77580153+OmarNasab@users.noreply.github.com>
2022-06-16 02:16:07 +0800

    Update index.blade.php

--- resources/views/Pages/Mission/index.blade.php
+++ resources/views/Pages/Mission/index.blade.php
@@ -132,29 +132,4 @@
             </div>
         </div>
     </div>
-    <script>
-        const tabElements = [
-            {
-                id: 'in-progress',
-                triggerEl: document.getElementById('in-progress-tab'),
-                targetEl: document.getElementById('in-progress')
-            },
-            {
-                id: 'completed',
-                triggerEl: document.getElementById('completed-tab'),
-                targetEl: document.getElementById('completed')
-            }
-        ];
-
-        // options with default values
-        const options = {
-            defaultTabId: 'settings',
-            activeClasses: 'text-blue-600 hover:text-blue-600 dark:text-blue-500 dark:hover:text-blue-400 border-blue-600 dark:border-blue-500',
-            inactiveClasses: 'text-gray-500 hover:text-gray-600 dark:text-gray-400 border-gray-100 hover:border-gray-300 dark:border-gray-700 dark:hover:text-gray-300',
-            onShow: () => {
-                console.log('tab is shown');
-            }
-        };
-        const tabs = new Tabs(tabElements, options);
-    </script>
 </x-app-layout>
