0b62f0ab0e8cdcfae134eb05f1db9eb673397d66
OmarNasab <77580153+OmarNasab@users.noreply.github.com>
2022-06-15 23:38:16 +0800

    Update tabs.js

--- public/js/tabs.js
+++ public/js/tabs.js
@@ -3,20 +3,20 @@ import Tabs from "flowbite"
 const tabElements = [
     {
         id: 'in-progress',
-        triggerEl: document.querySelector('#in-progress-tab'),
-        targetEl: document.querySelector('#in-progress')
+        triggerEl: document.getElementById('in-progress-tab'),
+        targetEl: document.getElementById('in-progress')
     },
     {
         id: 'completed',
-        triggerEl: document.querySelector('#completed-tab'),
-        targetEl: document.querySelector('#completed')
+        triggerEl: document.getElementById('completed-tab'),
+        targetEl: document.getElementById('completed')
     }
 ];
 
 // options with default values
 const options = {
     defaultTabId: 'settings',
-    activeClasses: 'text-red-200 hover:text-gray-600 dark:text-blue-500 dark:hover:text-blue-400 border-blue-600 dark:border-blue-500',
+    activeClasses: 'text-red-200 hover:text-gray-600 dark:text-blue-500 dark:hover:text-blue-400 border-gray-600 dark:border-blue-500',
     inactiveClasses: 'text-gray-500 hover:text-gray-600 dark:text-gray-400 border-gray-100 hover:border-gray-300 dark:border-gray-700 dark:hover:text-gray-300',
     onShow: () => {
         console.log('tab is shown');
