e69ebb74e59efe3741cb19cb9b49b0b62a9d0e1d
OmarNasab <77580153+OmarNasab@users.noreply.github.com>
2022-06-18 04:08:05 +0800

    changing expense show

--- resources/views/Pages/Expense/show.blade.php
+++ resources/views/Pages/Expense/show.blade.php
@@ -16,7 +16,7 @@
         </div>
     </div>
     <div class="container mx-auto mt-1 bg-white shadow ">
-        <div class="text-2xl py-4 border-b-2 border-blue-500">
+        <div class="text-2xl py-4 border-b-2 border-picton-blue">
             <div class="ml-4">
                 Expense Details
             </div>
@@ -27,7 +27,7 @@
                     <img class="inline" src="{{URL::asset("storage")."/".$expense->attachment}}" width="400" alt="document" >
                 </div>
                 <div class="row-span-1">
-                    <a type="button" href="{{route("downloadImage",[$expense->id])}}" target="_blank" class="text-white w-full text-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"><i class="fa-solid fa-cloud-arrow-down"></i> Download</a>
+                    <a type="button" href="{{route("downloadImage",[$expense->id])}}" target="_blank" class="text-white w-full text-center bg-picton-blue hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"><i class="fa-solid fa-cloud-arrow-down"></i> Download</a>
                 </div>
                 <div class="row-span-1"><span class="font-bold">Driver Name:</span> {{$expense->driver->full_name}}</div>
                 <div class="row-span-1"><span class="font-bold">Amount:</span> AED{{$expense->amount}}</div>
