f96c07ea185add31f095fd286d40be2c4c40b10e
OmarNasab <77580153+OmarNasab@users.noreply.github.com>
2022-06-18 04:59:55 +0800

    Update show.blade.php

--- resources/views/Pages/Mission/show.blade.php
+++ resources/views/Pages/Mission/show.blade.php
@@ -1,5 +1,5 @@
 <?php
-$directions=[];
+$directions = [];
 ?>
 @foreach($mission->direction as $stop)
     @foreach($stop["direction"] as $direction)
@@ -24,13 +24,13 @@ $directions=[];
                     </button>
                 </li>
                 @if(count($mission->direction)!=0)
-                <li class="mr-2" role="presentation">
-                    <button
-                        class="inline-block p-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
-                        id="direction-tab" type="button" role="tab"
-                        aria-controls="direction" aria-selected="false">Directions
-                    </button>
-                </li>
+                    <li class="mr-2" role="presentation">
+                        <button
+                            class="inline-block p-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
+                            id="direction-tab" type="button" role="tab"
+                            aria-controls="direction" aria-selected="false">Directions
+                        </button>
+                    </li>
                 @endif
             </ul>
         </div>
@@ -81,111 +81,123 @@ $directions=[];
                         </div>
                     </div>
                     @if(count($mission->direction)!=0)
-                    <div class="hidden p-4 bg-gray-50 rounded-lg dark:bg-gray-800" id="direction" role="tabpanel"
-                         aria-labelledby="direction-tab">
-                        <div id="map" style="height: 500px">
-                        </div>
-                        <label for="default-range" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Default range</label>
-                        <input id="default-range" type="range" value="1" min="1" max="{{count($mission->direction[0]["direction"])}}" onchange="changeMarker()" class="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700">
-                        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                            <tr>
-                                <th scope="col" class="px-6 py-3">
-                                    Destination
-                                </th>
-                                <th scope="col" class="px-6 py-3">
-                                    Supervisor
-                                </th>
-                                <th scope="col" class="px-6 py-3">
-                                    <span class="sr-only">View</span>
-                                </th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            @foreach($mission->direction as $index => $stop)
-                                <tr class="border-b dark:bg-gray-800 dark:border-gray-700 odd:bg-white even:bg-gray-50 odd:dark:bg-gray-800 even:dark:bg-gray-700">
-                                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
-                                        {{$stop["name"]}}
+                        <div class="hidden p-4 bg-gray-50 rounded-lg dark:bg-gray-800" id="direction" role="tabpanel"
+                             aria-labelledby="direction-tab">
+                            <div id="map" style="height: 500px">
+                            </div>
+                            <label for="default-range"
+                                   class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Default
+                                range</label>
+                            <input id="default-range" type="range" value="1" min="1"
+                                   max="{{count($mission->direction[0]["direction"])}}" onchange="changeMarker()"
+                                   class="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700">
+                            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                                <thead
+                                    class="text-xs text-gray-700 uppercase bg-gray-200 dark:bg-gray-700 dark:text-gray-400">
+                                <tr>
+                                    <th scope="col" class="px-6 py-3">
+                                        Destination
                                     </th>
-                                    <td class="px-6 py-4">
-                                    </td>
-                                    <td class="px-6 py-4 text-right">
-                                        <button onclick="changePolyLine({{$index}})" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">View</button>
-                                    </td>
+                                    <th scope="col" class="px-6 py-3">
+                                        Supervisor
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        <span class="sr-only">View</span>
+                                    </th>
                                 </tr>
-                            @endforeach
-                            </tbody>
-                        </table>
-                    </div>
+                                </thead>
+                                <tbody>
+                                @foreach($mission->direction as $index => $stop)
+                                    <tr class="border-b dark:bg-gray-800 dark:border-gray-700 odd:bg-white even:bg-gray-50 odd:dark:bg-gray-800 even:dark:bg-gray-700">
+                                        <th scope="row"
+                                            class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                                            {{$stop["name"]}}
+                                        </th>
+                                        <td class="px-6 py-4">
+                                        </td>
+                                        <td class="px-6 py-4 text-right">
+                                            <button onclick="changePolyLine({{$index}})"
+                                                    class="font-medium text-blue-600 dark:text-blue-500 hover:underline">
+                                                View
+                                            </button>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
                     @endif
                 </div>
             </div>
         </div>
     </div>
     @if(count($mission->direction)!=0)
-    <script>
-        var map;
-        var markersArray = [];
-        var polyLinesArray=[];
-        var paths=[];
-        var currentIndex=0;
-        function initMap() {
-            map = new google.maps.Map(document.getElementById("map"), {
-                zoom: 15,
-                center: {
-                    lat: {{$mission->direction[0]["direction"][0]["lat"]}},
-                    lng: {{$mission->direction[0]["direction"][0]["long"]}},
-                    mapTypeId: "terrain",
-                }
-            });
-            addMarker({{$directions[0][0]}},{{$directions[0][1]}})
-            let flightPath;
-            @foreach($mission->direction as $stop)
-                var path=[  @foreach($stop["direction"] as $direction)
-                    {
-                        lat: {{$direction["lat"]}},lng: {{$direction["long"]}}
+        <script>
+            var map;
+            var markersArray = [];
+            var polyLinesArray = [];
+            var paths = [];
+            var currentIndex = 0;
 
-                    },
-                @endforeach
+            function initMap() {
+                map = new google.maps.Map(document.getElementById("map"), {
+                    zoom: 15,
+                    center: {
+                        lat: {{$mission->direction[0]["direction"][0]["lat"]}},
+                        lng: {{$mission->direction[0]["direction"][0]["long"]}},
+                        mapTypeId: "terrain",
+                    }
+                });
+                addMarker({{$directions[0][0]}}, {{$directions[0][1]}})
+                let flightPath;
+                @foreach($mission->direction as $stop)
+                var path = [@foreach($stop["direction"] as $direction)
+                {
+                    lat: {{$direction["lat"]}}, lng: {{$direction["long"]}}
+
+                },
+                    @endforeach
                 ]
-             flightPath = new google.maps.Polyline({
-                path:path,
-                geodesic: true,
-                strokeColor: "#FF0000",
-                strokeOpacity: 1.0,
-                strokeWeight: 2,
-            });
+                flightPath = new google.maps.Polyline({
+                    path: path,
+                    geodesic: true,
+                    strokeColor: "#FF0000",
+                    strokeOpacity: 1.0,
+                    strokeWeight: 2,
+                });
                 paths.push(path)
-            polyLinesArray.push(flightPath)
-            @endforeach
-            polyLinesArray[0].setMap(map);
-        }
-
-        function addMarker(lat,long) {5
-             marker = new google.maps.Marker({
-                position: new google.maps.LatLng(lat,long),
-                map: map
-            })
-            markersArray.push(marker)
-        }
-
-        function changeMarker(){
-            markersArray[0].setMap(null)
-            markersArray.length=0;
-            let value=document.getElementById("default-range").value;
-            addMarker(paths[currentIndex][value-1]["lat"],paths[currentIndex][value-1]["lng"])
-        }
-        function changePolyLine(index){
-            for(let i=0;i<polyLinesArray.length;i++){
-                polyLinesArray[i].setMap(null)
+                polyLinesArray.push(flightPath)
+                @endforeach
+                    polyLinesArray[0].setMap(map);
             }
-            currentIndex=index;
-            document.getElementById("default-range").value=1;
-            document.getElementById("default-range").setAttribute("max",paths[index].length)
-            polyLinesArray[index].setMap(map)
-            changeMarker()
-        }
-    </script>
+
+            function addMarker(lat, long) {
+                5
+                marker = new google.maps.Marker({
+                    position: new google.maps.LatLng(lat, long),
+                    map: map
+                })
+                markersArray.push(marker)
+            }
+
+            function changeMarker() {
+                markersArray[0].setMap(null)
+                markersArray.length = 0;
+                let value = document.getElementById("default-range").value;
+                addMarker(paths[currentIndex][value - 1]["lat"], paths[currentIndex][value - 1]["lng"])
+            }
+
+            function changePolyLine(index) {
+                for (let i = 0; i < polyLinesArray.length; i++) {
+                    polyLinesArray[i].setMap(null)
+                }
+                currentIndex = index;
+                document.getElementById("default-range").value = 1;
+                document.getElementById("default-range").setAttribute("max", paths[index].length)
+                polyLinesArray[index].setMap(map)
+                changeMarker()
+            }
+        </script>
         <script>
             const tabElements = [
                 {
@@ -211,10 +223,10 @@ $directions=[];
             };
             const tabs = new Tabs(tabElements, options);
         </script>
-    <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
-    <script
-        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDAMFxLg6Qi_iib6bKjJIxyEmOPvUr7m_Y&callback=initMap&v=weekly"
-        async
-    ></script>
+        <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
+        <script
+            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDAMFxLg6Qi_iib6bKjJIxyEmOPvUr7m_Y&callback=initMap&v=weekly"
+            async
+        ></script>
     @endif
 </x-app-layout>
