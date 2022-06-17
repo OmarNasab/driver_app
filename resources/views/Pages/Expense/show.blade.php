9edc15bf9c4ec7a3f15019af5ede3b8b2f974d66
OmarNasab <77580153+OmarNasab@users.noreply.github.com>
2022-06-18 04:01:17 +0800

    Update show.blade.php

--- resources/views/Pages/Expense/show.blade.php
+++ resources/views/Pages/Expense/show.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-    <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+    <div class="max-w-7xl mx-auto py-6 sm:px-6">
         <div class="flex flex-row w-full">
             @if($expense->status==0)
                 <a type="button"
