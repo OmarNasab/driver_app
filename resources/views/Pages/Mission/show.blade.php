c888a2c791edf255125fc33bb3f7aaf12a5aa61d
OmarNasab <77580153+OmarNasab@users.noreply.github.com>
2022-06-23 00:24:45 +0800

    Update show.blade.php

--- resources/views/Pages/Mission/show.blade.php
+++ resources/views/Pages/Mission/show.blade.php
@@ -118,8 +118,7 @@ $directions = [];
                             <div id="map" style="height: 500px">
                             </div>
                             <label for="default-range"
-                                   class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Default
-                                range</label>
+                                   class="block text-center mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Time: </label>
                             <input id="default-range" type="range" value="1" min="1"
                                    max="{{count($mission->direction[0]["direction"])}}" onchange="changeMarker()"
                                    class="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700">
