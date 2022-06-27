440c5645d06b9c149058f54be510a39d6a5e009d
OmarNasab <77580153+OmarNasab@users.noreply.github.com>
2022-06-27 11:04:35 +0300

    Update show.blade.php

--- resources/views/Pages/Mission/show.blade.php
+++ resources/views/Pages/Mission/show.blade.php
@@ -83,15 +83,12 @@ $directions = [];
                                 </div>
                             @endif
                         </div>
-                        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                        <table class="w-1/2 text-sm text-left text-gray-500 dark:text-gray-400">
                             <thead
                                 class="text-xs text-gray-700 uppercase bg-gray-200 dark:bg-gray-700 dark:text-gray-400">
                             <tr>
                                 <th scope="col" class="px-6 py-3">
                                     Destination
-                                </th>
-                                <th scope="col" class="px-6 py-3">
-                                    <span class="sr-only">View</span>
                                 </th>
                             </tr>
                             </thead>
@@ -102,12 +99,6 @@ $directions = [];
                                         class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
                                         {{$stop["name"]}}
                                     </th>
-                                    <td class="px-6 py-4 text-right">
-                                        <button onclick="changePolyLine({{$index}})"
-                                                class="font-medium text-blue-600 dark:text-blue-500 hover:underline">
-                                            View
-                                        </button>
-                                    </td>
                                 </tr>
                             @endforeach
                             </tbody>
